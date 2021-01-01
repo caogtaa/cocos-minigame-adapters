d7d24a1c5d1b0ec4932204221c9669a323c29fc3
Cao Gaoting <caogtaa@gmail.com>
2021-01-01 15:41:56 +0800

    use async localStorage writing

--- platforms/wechat/wrapper/builtin/localStorage.js
+++ platforms/wechat/wrapper/builtin/localStorage.js
@@ -1,3 +1,12 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-31 01:06:49
+ * @LastEditTime: 2021-01-01 15:41:08
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \adapters\platforms\wechat\wrapper\builtin\localStorage.js
+ */
+
 const localStorage = {
   get length() {
     const { keys } = wx.getStorageInfoSync()
@@ -16,15 +25,25 @@ const localStorage = {
   },
 
   setItem(key, value) {
-    return wx.setStorageSync(key, value)
+    // return wx.setStorageSync(key, value)
+    // 20210101, 改为异步写入, 因为业务上没有同时读写的需求。这里假设wx接口的写入是异步排队的
+    // console.log(`enter, key = ${key}, data = ${value} `);
+    return wx.setStorage({
+      key: key,
+      data: value
+    });
   },
 
   removeItem(key) {
-    wx.removeStorageSync(key)
+    // wx.removeStorageSync(key)
+    wx.removeStorage(key);
+    // console.log(`-- remove key ${key}`);
   },
 
   clear() {
-    wx.clearStorageSync()
+    wx.clearStorage();
+    // console.log(`-- clear storage ${key}`);
+    // wx.clearStorageSync()
   }
 }
 
