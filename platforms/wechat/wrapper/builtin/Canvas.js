9963222f06cc9ca9c313cc8bd0bbe1c6c3aef89a
Cao Gaoting <caogtaa@gmail.com>
2021-11-10 10:29:39 +0800

    custom boot scene for wx adapter

--- platforms/wechat/wrapper/builtin/Canvas.js
+++ platforms/wechat/wrapper/builtin/Canvas.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-10 10:11:24
+ * @LastEditTime: 2021-11-10 10:27:41
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \adapters\platforms\wechat\wrapper\builtin\Canvas.js
+ */
 // import HTMLCanvasElement from './HTMLCanvasElement'
 import { innerWidth, innerHeight } from './WindowProperties'
 
@@ -5,8 +13,14 @@ let hasModifiedCanvasPrototype = false
 let hasInit2DContextConstructor = false
 let hasInitWebGLContextConstructor = false
 
+// https://forum.cocos.org/t/cocos-creator-50/94999
+// 首场景引擎剥离
+let first = true
 export default function Canvas() {
-  const canvas = wx.createCanvas()
+  // const canvas = wx.createCanvas()
+  const gt_data = wx.__gt_data || {};
+  const canvas = first ? gt_data.first_canvas ? gt_data.first_canvas : wx.createCanvas() : wx.createCanvas()
+  first = false
 
   canvas.type = 'canvas'
 
--- platforms/wechat/wrapper/builtin/localStorage.js
+++ platforms/wechat/wrapper/builtin/localStorage.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-11-10 10:11:24
+ * @LastEditTime: 2021-11-10 10:28:15
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \adapters\platforms\wechat\wrapper\builtin\localStorage.js
+ */
 const localStorage = {
   get length() {
     const { keys } = wx.getStorageInfoSync()
@@ -16,15 +24,25 @@ const localStorage = {
   },
 
   setItem(key, value) {
-    return wx.setStorageSync(key, value)
+    // return wx.setStorageSync(key, value)
+    // 20210101, 改为异步写入, 因为业务上没有同时读写的需求。这里假设wx接口的写入是异步排队的
+    // console.log(`enter, key = ${key}, data = ${value} `);
+    return wx.setStorage({
+      key: key,
+      data: value
+    });
   },
 
   removeItem(key) {
-    wx.removeStorageSync(key)
+    // wx.removeStorageSync(key)
+    wx.removeStorage(key);
+    // console.log(`-- remove key ${key}`);
   },
 
   clear() {
-    wx.clearStorageSync()
+    wx.clearStorage();
+    // console.log(`-- clear storage ${key}`);
+    // wx.clearStorageSync()
   }
 }
 
